96f48930aa7c5e83a987b3d6ea118009a1bacebf
Michael Edie <34865864+c0demech@users.noreply.github.com>
2018-02-09 08:44:01 -0500

    Create lemar.sh

---
+++ lemar.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+#
+## c0demech // 02.08.18
+## CCTC Linux Challenge
+###################
+
+function do_gz () {
+  echo "Gzip extraction"
+  mv $1 "$1".gz
+  gzip -dq "$1".gz
+}
+
+function do_bz () {
+  echo "Bzip extraction"
+}
+
+function do_b2 () {
+  echo "Bz2 extraction"
+  bzip2 -dq $1
+}
+
+function untar () {
+  echo "Tar extraction"
+  tar xf $1
+  rm $1
+}
+
+function scrape () {
+  for i in *
+    do
+      filetype=$(file $i)
+      case $filetype in
+        *bzip2*)
+          do_b2 $i
+          ;;
+        *gzip*)
+          do_gz $i
+          ;;
+        *tar*)
+          untar $i
+          ;;
+        *)
+          echo "END END END"
+          cat $i
+          exit 0
+          ;;
+      esac
+    done
+}
+
+while :
+  do
+    scrape
+done
